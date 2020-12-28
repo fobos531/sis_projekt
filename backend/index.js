5d22780b48a63152980eaf65d4df9093182cc097
fifi98 <bel.filip@hotmail.com>
2020-12-28 11:56:53 +0100

    Changed Express server name for CORS example

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const appCorsNeDela = express();
+const appCORS = express();
 const routes = require("./routes/");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -39,14 +39,18 @@ app.get("*", csrfProtection, (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
-appCorsNeDela.use(cors({ origin: "http://172.217.18.78:3000" }));
-appCorsNeDela.use(express.json());
-appCorsNeDela.use("/api", routes);
+appCORS.use(cors({ origin: "http://172.217.18.78:3000" }));
+appCORS.use(express.json());
+appCORS.use("/api", routes);
 
-const httpServer = http.createServer(app).listen(8080, () => console.log("HTTP server started on port 8080!"));
-const httpCorsNeDelaServer = http
-  .createServer(appCorsNeDela)
-  .listen(8081, () => console.log("HTTP server with strict CORS started on port 8081!"));
-const httpsServer = https
-  .createServer({ key: fs.readFileSync("sis_projekt_selfsigned.key"), cert: fs.readFileSync("sis_projekt_selfsigned.crt") }, app)
+http.createServer(app).listen(8080, () => console.log("HTTP server started on port 8080!"));
+http.createServer(appCORS).listen(8081, () => console.log("HTTP server with strict CORS started on port 8081!"));
+https
+  .createServer(
+    {
+      key: fs.readFileSync("sis_projekt_selfsigned.key"),
+      cert: fs.readFileSync("sis_projekt_selfsigned.crt"),
+    },
+    app
+  )
   .listen(8443, () => console.log("HTTPS server started on port 8443"));
